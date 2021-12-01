0e013454653c39010af3c70020eaaeed9959ea06
Gray <>
2021-12-01 14:47:10 +0600

    initial commit

---
+++ Testing_app-ForGitUITests/Testing_app_ForGitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Testing_app_ForGitUITestsLaunchTests.swift
+//  Testing_app-ForGitUITests
+//
+//  Created by ablai erzhanov on 01.12.2021.
+//
+
+import XCTest
+
+class Testing_app_ForGitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
